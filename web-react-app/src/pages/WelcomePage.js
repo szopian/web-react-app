0ebed967e5c58dd42e33c40998c4093d64a5f011
Adri Ana <szopiangreg@gmail.com>
2021-05-13 22:20:38 +0100

    add style to welcome page

--- web-react-app/src/pages/WelcomePage.js
+++ web-react-app/src/pages/WelcomePage.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
+import styled from "styled-components";
+
+const Page = styled.div`
+  height: 100vh;
+  width: 100%;
+  backgroud: pink;
+`;
 
 function WelcomePage() {
   return (
-    <div>
+    <Page>
       <Header />
       <Navbar />
-    </div>
+    </Page>
   );
 }
 

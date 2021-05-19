b5fbe6dff747d4f718076e81f44918bd38210c1f
Adri Ana <szopiangreg@gmail.com>
2021-05-19 11:48:24 +0100

    working on mouse events with reackt hooks

--- web-react-app/src/pages/WelcomePage.js
+++ web-react-app/src/pages/WelcomePage.js
@@ -1,22 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
-import Navbar from "../components/Navbar";
 import styled from "styled-components";
 import Image from "../images/mattias-olsson-nQz49efZEFs-unsplash.jpg";
 
-const Page = styled.div`
-  height: 100vh;
-  background-image: url(${Image});
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
+function WelcomePage() {
+  const [backgroundImage, setBackgroundImage] = useState(Image);
+
+  const setStyle = (image) => {
+    setBackgroundImage(image);
+  };
+
+  const Page = styled.div`
+    height: 100vh;
+    background-image: url(${backgroundImage});
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  `;
+
+  const NavbarContainer = styled.div`
+    box-sizing: border-box;
+    position: absolute;
+    top: 50%;
+    left: 13.5vw;
+    width: calc(100% - 13.5vw);
+  `;
+
+  const Nav = styled.nav`
+  font-size: 40px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: -0.02em;
+  line-height: 1em;
+  text-transform: none;
+  padding-right: 0.5em;
+  padding-left: 0;
+  margin: 0 auto;
+  color: white;
+  position: relative;
+  }
 `;
 
-function WelcomePage() {
+  const NavItem = styled.div`
+    display: block;
+    margin: 0 auto;
+    cursor: pointer;
+    padding: 5px;
+  `;
+
+  const NavLink = styled.a`
+    transition: 0.3s;
+
+    &:hover {
+      opacity: 0.5;
+    }
+  `;
+
   return (
     <Page>
       <Header />
-      <Navbar />
+      <NavbarContainer>
+        <Nav>
+          <NavItem>
+            <NavLink>About</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink>Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink>Products</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink>Work</NavLink>
+          </NavItem>
+        </Nav>
+      </NavbarContainer>
     </Page>
   );
 }

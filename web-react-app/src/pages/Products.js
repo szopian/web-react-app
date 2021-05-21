270f1109a08a4900b0571b38ec36ba2fe719999f
Adri Ana <szopiangreg@gmail.com>
2021-05-21 13:28:59 +0100

    swork on pages

--- web-react-app/src/pages/Products.js
+++ web-react-app/src/pages/Products.js
@@ -1,10 +1,46 @@
 import React from "react";
+import Header from "../components/Header";
+import styled from "styled-components";
+import Image from "../images/j-gowell-Lp8wAUBB1x8-unsplash.jpg";
+
+const Page = styled.div`
+  background: #000;
+  height: 100vh;
+  width: 100%;
+  color: #fff;
+  background-image: url(${Image});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+`;
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.9);
+
+  /* changes images view 
+  background: linear-gradient(red, transparent),
+    linear-gradient(to top left, lime, transparent),
+    linear-gradient(to top right, blue, transparent);
+  */
+
+  mix-blend-mode: overlay;
+  background-blend-mode: screen;
+`;
+
+const Text = styled.div``;
 
 function Products() {
   return (
-    <div>
-      <h1>Hi Products!!</h1>
-    </div>
+    <Page>
+      <Overlay />
+      <Header />
+      <Text></Text>
+    </Page>
   );
 }
 

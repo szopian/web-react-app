a05f5de53fb9efb8c07130d6de53f209c1f4a2d4
Adri Ana <szopiangreg@gmail.com>
2021-05-24 17:30:11 +0100

    add text to home page

--- web-react-app/src/pages/Home.js
+++ web-react-app/src/pages/Home.js
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import styled from "styled-components";
 import Image from "../images/robson-hatsukami-morgan-5C6veSN6hec-unsplash.jpg";
 import Title from "../components/Title";
+import Text from "../components/Text";
 
 const Page = styled.div`
   background: #000;
@@ -39,6 +40,7 @@ function Home() {
       <Overlay />
       <Header />
       <Title title="Home" />
+      <Text text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, facere eum, accusamus iusto soluta reiciendis ipsum voluptates ad perspiciatis alias esse voluptatem natus debitis a unde numquam, exercitationem modi delectus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, facere eum, accusamus iusto soluta reiciendis ipsum voluptates ad perspiciatis alias esse voluptatem natus debitis a unde numquam, exercitationem modi delectus?" />
     </Page>
   );
 }

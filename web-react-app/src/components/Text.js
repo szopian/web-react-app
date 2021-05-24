ffa20b3207825b6bdd61c0570d61250cbfaeadd4
Adri Ana <szopiangreg@gmail.com>
2021-05-24 14:34:04 +0100

    add changes

--- web-react-app/src/components/Text.js
+++ web-react-app/src/components/Text.js
@@ -1,12 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 
-const TextContainer = styled.h2``;
+const TextContainer = styled.h3``;
 
 const Text = ({ text }) => {
   return (
     <TextContainer>
-      <h2>{text}</h2>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga saepe totam
+      vel, explicabo consequuntur non repudiandae suscipit dolore nobis nulla
+      itaque est deserunt laudantium, quas ea, aperiam eius modi velit.
     </TextContainer>
   );
 };
--- web-react-app/src/components/Title.js
+++ web-react-app/src/components/Title.js
@@ -1,13 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 
-const TitleText = styled.div`
+const TitleText = styled.h1`
   margin: 0;
   position: absolute;
   top: 50%;
   left: 33%;
   transform: translate(-50%, -50%);
-  font-size: 50px;
+  font-size: 5em;
   background: linear-gradient(transparent 50%, rgba(0, 0, 0, 0.85) 50%);
   padding: 20px;
   blur: 5px;
